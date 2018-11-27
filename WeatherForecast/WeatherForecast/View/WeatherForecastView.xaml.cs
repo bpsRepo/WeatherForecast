59815dc3e572c5529a76e97cb0dfb569b0306565
bpsRepo <45334572+bpsRepo@users.noreply.github.com>
2018-11-27 14:59:46 +0100

    View has been created

---
+++ WeatherForecast/WeatherForecast/View/WeatherForecastView.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using WeatherForecast.ViewModel;
+
+namespace WeatherForecast.View
+{
+    /// <summary>
+    /// Interaction logic for WeatherForecastView.xaml
+    /// </summary>
+    public partial class WeatherForecastView : Window
+    {
+        /// <summary>
+        /// Weather forecast view constructor
+        /// </summary>
+        /// <param name="weatherForecastVM">Viemodel for weather forecast view</param>
+        public WeatherForecastView(WeatherForecastVM weatherForecastVM)
+        {
+            InitializeComponent();
+            this.DataContext = weatherForecastVM;
+        }
+    }
+}
