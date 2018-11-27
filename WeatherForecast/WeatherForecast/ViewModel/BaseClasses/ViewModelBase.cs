0f0d84f913a1802a7a438145c7c708b413e802b8
bpsRepo <45334572+bpsRepo@users.noreply.github.com>
2018-11-27 15:00:00 +0100

    ViewModel has been created

---
+++ WeatherForecast/WeatherForecast/ViewModel/BaseClasses/ViewModelBase.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WeatherForecast.ViewModel.BaseClasses
+{
+    /// <summary>
+    /// Base ViewModel class for the ViewModels
+    /// </summary>
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ WeatherForecast/WeatherForecast/ViewModel/WeatherForecastVM.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.ObjectModel;
+using WeatherForecast.DAL;
+using WeatherForecast.Model;
+using WeatherForecast.ViewModel.BaseClasses;
+using System.Threading.Tasks;
+
+namespace WeatherForecast.ViewModel
+{
+    /// <summary>
+    /// Weather Forecast ViewModel
+    /// </summary>
+    public class WeatherForecastVM : ViewModelBase
+    {
+
+        #region Parameters
+
+        private ObservableCollection<List> list;
+
+        private List selectedListItem;
+
+        #endregion
+
+        #region Properties
+
+
+        public ObservableCollection<List> List
+        {
+            get { return list; }
+            set
+            {
+                list = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public List SelectedListItem
+        {
+            get { return selectedListItem; }
+            set
+            {
+                selectedListItem = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+    }
+}
