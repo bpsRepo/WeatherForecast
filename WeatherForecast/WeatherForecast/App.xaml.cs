03a503b063fde5b98593d294affba4d98876fa18
bpsRepo <45334572+bpsRepo@users.noreply.github.com>
2018-11-27 14:59:19 +0100

    Application_Startup method implemented

--- WeatherForecast/WeatherForecast/App.xaml.cs
+++ WeatherForecast/WeatherForecast/App.xaml.cs
@@ -5,6 +5,9 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using WeatherForecast.DAL;
+using WeatherForecast.View;
+using WeatherForecast.ViewModel;
 
 namespace WeatherForecast
 {
@@ -13,5 +16,25 @@ namespace WeatherForecast
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            try
+            {
+                WeatherForecastVM weatherForecastVM = new WeatherForecastVM();
+
+                IDataCollector dataCollector = new LiveDataCollector();
+
+                IDataParser dataParser = new JSONParser();
+                weatherForecastVM.List = new System.Collections.ObjectModel.ObservableCollection<Model.List>(dataParser.ParseWeatherForecastData(dataCollector.Collect()).list);
+
+                WeatherForecastView weatherForecastView = new WeatherForecastView(weatherForecastVM);
+
+                weatherForecastView.Show();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Application chrased." + ex.Message, "Error", System.Windows.MessageBoxButton.OK, System.Windows.MessageBoxImage.Error);
+            }
+        }
     }
 }
